42bd8fff044a8e8bd1a646076108ddef7f75e1a9
andrea5972 <andrealindamurphy@gmail.com>
2018-02-06 19:54:14 -0500

    Adding week3 project for commit

---
+++ django/week3/portfolio/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+def home(request):
+    """
+    Renders the home page for the web application portfolio "Andrea Murphy"
+    Retuns: the home.html page
+    """
+    context = {}
+    return render(request, 'home.html', context)
+
+
+def resume(request):
+    """
+    Renders my Resume static page template
+    Retuns: the resume.html page
+    """
+    context = {}
+    return render(request, 'resume.html', context)
+
+
+def portfolio(request):
+    """
+    Renders my Portfolio static page template
+    Retuns: the portfolio.html page
+    """
+    context = {}
+    return render(request, 'portfolio.html', context)
+
+
+def contact(request):
+    """
+    Renders my Contact static page template
+    Retuns: the contact.html page
+    """
+    context = {}
+    return render(request, 'contact.html', context)
