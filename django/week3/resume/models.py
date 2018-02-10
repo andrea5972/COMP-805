d83bfa9a3cde3d0068104ba9322d96bba350a4b8
andrea5972 <andrealindamurphy@gmail.com>
2018-02-10 16:53:10 -0500

    Adding models.py for commit

--- django/week3/resume/models.py
+++ django/week3/resume/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Experience(models.Model):
+    """
+    Model (DB layer) for Andrea Murphy professional work experience
+    """
+    title = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.CharField(max_length=40)
+
+
+    def __str__(self):
+            return self.title
+
+class Education(models.Model):
+    """
+    Model (DB layer) for Andrea Murphy's education records
+
+    """
+    institution_name = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    degree = models.CharField(max_length=40)
+    major = models.CharField(max_length=40)
+    gpa = models.FloatField(max_length=40)
+
+    def __str__(self):
+        return "{} , {}".format(self.front, self.back)
