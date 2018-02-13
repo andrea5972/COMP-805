651da9962ca04ad7797235c051a81497ad61da24
andrea5972 <andrealindamurphy@gmail.com>
2018-02-13 15:30:48 -0500

    Adding finial commit and updates to week3 django project

--- django/week3/resume/models.py
+++ django/week3/resume/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Education(models.Model):
     """
@@ -11,12 +12,22 @@ class Education(models.Model):
     major = models.CharField(max_length=40, null=False, blank=True)
     gpa = models.FloatField(max_length=40, null=False, blank=True)
 
-
     def __str__(self):
         """
-        String for representing the Model object.
+        Returns fields attached to Education models.
         """
-        return self.institution_name, location, degree, major, gpa
+        if not hasattr(self, 'model'):
+            return super().__str__()
+        model = self.Education
+        app = Education._meta.app_label
+        return '%s.%s.%s' % (resume, Education._meta.object_name, self.name)
+
+
+class EducationForm(ModelForm):
+    class Meta:
+       model = Education
+       fields = ['institution_name', 'location', 'degree', 'major', 'gpa']
+
 
 class Experience(models.Model):
     """
@@ -29,9 +40,19 @@ class Experience(models.Model):
     end_date = models.DateField()
     description = models.TextField()
 
-
     def __str__(self):
-            return self.company, title, location, start_date, end_date, description
+        """
+        Returns fields attached to Experience models.
+        """
+        if not hasattr(self, 'model'):
+            return super().__str__()
+        model = self.Experience
+        app = Experience._meta.app_label
+        return '%s.%s.%s' % (resume, Experience._meta.object_name, self.name)
 
+class ExperienceForm(ModelForm):
+    class Meta:
+       model = Experience
+       fields = ['company', 'title', 'location', 'start_date', 'end_date', 'description']
 
 
--- django/week3/resume/views.py
+++ django/week3/resume/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+
 from django.template import loader
 
 
+from django.forms import modelformset_factory
 from .models import Education, Experience
 
 def index(request):
@@ -18,7 +21,20 @@ def index(request):
     context = {'new_education':qs1, 'new_experience': qs2 }
     return render(request, 'resume/home.html', context)
 
+def manage_education(request):
+    """
+    """
+    EducationFormSet = modelformset_factory(Education, fields=('institution_name', 'location', 'degree', 'major', 'gpa'))
+    if request.method == 'POST':
+        formset = Education.FormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save()
+    else:
+        formset = EducationFormSet()
+    return render(request,  'resume/education.html', {'formset': formset})
 
-
-
-
+def get_field(self):
+    """
+    """
+    new_education = Education.objects.get(structidx__exact=id).get_fields()
+    return render_to_response('resume/home.html', { 'new_education' : new_education}, context_instance = RequestContext(request))
