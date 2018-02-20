a6f6f5621d65a4d635587fd2cc57b86e7701a7fc
andrea5972 <andrealindamurphy@gmail.com>
2018-02-19 19:54:13 -0500

    Adding unittesting test.py file for commit

--- django/week4/resume/tests.py
+++ django/week4/resume/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from resume.models import Resume, Experience, Education
 
-# Create your tests here.
+
+class ResumeTestCase(TestCase):
+
+    def setUp(self):
+        """
+        Setup is ran before every test case
+        For each Model create objects for test database
+        """
+        Resume.objects.create(First_name="Andrea", Last_name ="Murphy")
+        Experience.objects.create(
+                                                    company="Three Owl Design",
+                                                    title="Owner",
+                                                    location= "New Boston"
+                                                    )
+        Education.objects.create(
+                                                    institution_name="UNH",
+                                                    location="Manchester",
+                                                    major="MSIT",
+                                                    degree="Master",
+                                                    gpa= "4.0"
+                                                    )
+
+    def test_db(self):
+        """
+        Testing the setUp for our test DB
+        """
+        resume = Resume.objects.get(First_name="Andrea")
+        self.assertIsInstance(resume, Resume)
+
+
+    def test_get_full_name(self):
+        """
+        Testing the method get_full_name
+        """
+        resume = Resume.objects.get( First_name= 'Andrea')
+        expected = 'Andrea Murphy'
+        actual = resume.get_full_name()
+
+        self.assertEqual( expected, actual)
+
+    def test_get_experience(self):
+        """
+        Testing the method get_experience
+        """
+        resume = Resume.objects.first()
+        expected = list(resume.experience_set.all())
+        actual = list(resume.get_experience())
+
+        self.assertEqual( expected, actual )
+
+    def test_get_education(self):
+        """
+        Testing the method get_education
+        """
+        resume = Resume.objects.first()
+        expected = list(resume.education_set.all())
+        actual = list(resume.get_education())
+
+        self.assertEqual( expected, actual )
