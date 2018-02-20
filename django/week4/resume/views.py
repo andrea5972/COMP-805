e39fb162b06ea8739b3616125ff658bf652709a0
andrea5972 <andrealindamurphy@gmail.com>
2018-02-19 19:52:19 -0500

    Adding new views.py for week4

--- django/week4/resume/views.py
+++ django/week4/resume/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
-
 from django.template import loader
 
-
-from django.forms import modelformset_factory
-from .models import Education, Experience
+from .models import Resume
 
 def index(request):
     """
@@ -15,26 +9,9 @@ def index(request):
     Context: Education in query set
     Returns: 'resume/home.html'
     """
-    qs1 = Education.objects.all()
-    qs2 = Experience.objects.all()
 
-    context = {'new_education':qs1, 'new_experience': qs2 }
+    resume = Resume.objects.first()
+
+    context = { 'resume':resume }
     return render(request, 'resume/home.html', context)
 
-def manage_education(request):
-    """
-    """
-    EducationFormSet = modelformset_factory(Education, fields=('institution_name', 'location', 'degree', 'major', 'gpa'))
-    if request.method == 'POST':
-        formset = Education.FormSet(request.POST, request.FILES)
-        if formset.is_valid():
-            formset.save()
-    else:
-        formset = EducationFormSet()
-    return render(request,  'resume/education.html', {'formset': formset})
-
-def get_field(self):
-    """
-    """
-    new_education = Education.objects.get(structidx__exact=id).get_fields()
-    return render_to_response('resume/home.html', { 'new_education' : new_education}, context_instance = RequestContext(request))
